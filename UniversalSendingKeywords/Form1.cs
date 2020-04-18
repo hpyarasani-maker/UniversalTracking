8f1454266963ad8e934526881b4da8f8c1b36e40
Hemachander Pyarasani <44903980+hemachanderpyarasani@users.noreply.github.com>
2020-04-18 16:32:16 +0100

    update Sending Project

---
+++ UniversalSendingKeywords/Form1.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Threading;
+using System.Xml;
+
+namespace UniversalSendingKeywords
+{
+    public partial class Form1 : Form
+    {
+        SendingKeywordRequest WOWS = new SendingKeywordRequest();
+        System.Windows.Forms.Timer timer = new System.Windows.Forms.Timer();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Text = "D_YahooHK_Sending";
+
+            date_picker.Value = DateTime.Today; //.AddDays(-1);
+
+            Thread t = new Thread(new ThreadStart(mainLoop));
+            generateWorklist();
+            if (getWorklistSize() > 0)
+            {
+                t.Start();
+            }
+            else
+            {
+                t.Abort();
+                Close();
+            }
+        }
+
+        public void mainLoop()
+        {
+            while (getWorklistSize() > 0)
+            {
+                processWorklist();
+                generateWorklist();
+            }
+
+            Environment.Exit(Environment.ExitCode);
+        }
+
+        public string strConn()
+        {
+            try
+            {
+                XmlDocument xml = new XmlDocument();
+                string fileName = @"C:\Inetpub\wwwroot\Callback_TrackingTrending.xml";
+                // You'll need to put the correct path to your xml file here
+                xml.Load(fileName);
+
+                // Select a specific node
+                XmlNode node = xml.SelectSingleNode("ConnectionString/con");
+                // Get its value
+                string name = node.InnerText;
+
+                return name;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public void generateWorklist()
+        {
+            this.Invoke((MethodInvoker)delegate ()
+            {
+                worklist.Items.Clear();
+                worklist.Items.Add("38:ipad, iphone");
+                worklist.Refresh();
+                date_picker.Format = DateTimePickerFormat.Custom;
+                date_picker.CustomFormat = "yyyy-MM-dd";
+            });
+
+            return;
+
+            Cursor.Current = Cursors.WaitCursor;
+            string myDate = date_picker.Text;
+
+            string strQry = "exec [dbo].[Getkeywords_48] '" + myDate + "'";
+
+            SqlConnection objCon = null;
+            SqlDataReader objData = null;
+            try
+            {
+                objCon = new SqlConnection(strConn());
+                objCon.Open();
+                SqlCommand objCmd = new SqlCommand(strQry, objCon);
+                objCmd.CommandTimeout = 0;
+                objData = objCmd.ExecuteReader(CommandBehavior.CloseConnection);
+                while (objData.Read())
+                {
+                    this.Invoke((MethodInvoker)delegate ()
+                    {
+                        worklist.Items.Add(objData[0].ToString() + ":" + objData[1].ToString());
+                    });
+                }
+                this.Invoke((MethodInvoker)delegate ()
+                {
+                    worklist.Refresh();
+                });
+                objData.Close();
+            }
+            catch (SqlException e)
+            {
+                string errMsg = "Database Connection is temporarily not working\n" + e.ToString();
+                errorList.Invoke((MethodInvoker)delegate ()
+                {
+                    errorList.Items.Add(errMsg);
+                });
+            }
+            catch (Exception ex)
+            {
+                errorList.Invoke((MethodInvoker)delegate ()
+                {
+                    errorList.Items.Add(ex.ToString());
+                });
+            }
+
+            finally
+            {
+                objCon.Dispose();
+                objCon.Close();
+            }
+        }
+
+        public int getWorklistSize()
+        {
+            int worklistSize = worklist.Items.Count;
+            return worklistSize;
+        }
+
+        public void processWorklist()
+        {
+            string resultsString;
+            char sep;
+            Array resultsArray;
+            string seid;
+            string kn;
+
+            for (int i = 0; i < worklist.Items.Count; i++)
+            {
+                // get next Project ID, Search Engine ID, Keyword ID and Keyword from worklist
+                resultsString = worklist.Items[i].ToString();
+                sep = ':';
+                resultsArray = resultsString.Split(sep);
+                seid = resultsArray.GetValue(0).ToString();
+                kn = resultsArray.GetValue(1).ToString();
+                TimeSpan ts = new TimeSpan();
+                DateTime dt = DateTime.Now;
+                try
+                {
+                    processResults(seid, kn);
+                }
+                catch (Exception ex)
+                {
+                    errorList.Invoke((MethodInvoker)delegate ()
+                    {
+                        errorList.Items.Add(ex.Message);
+                    });
+                }
+                ts = DateTime.Now - dt;
+
+                this.Invoke((MethodInvoker)delegate ()
+                {
+                    progress_lbl.Text = "Completed : " + (i + 1) + " of " + worklist.Items.Count;
+                    results.Text = worklist.Items[i].ToString();
+                    progress_lbl.Refresh();
+                    lblIP.Text = ts.TotalSeconds.ToString();
+                });
+                //Thread.Sleep(60000);
+            }
+        }
+
+        public void processResults(string seid, string kn)
+        {
+            try
+            {
+                WOWS.getTop100(kn, Convert.ToInt32(seid));
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Environment.Exit(Environment.ExitCode);
+        }
+    }
+}
---
+++ UniversalSendingKeywords/OxyParams.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UniversalSendingKeywords
+{
+    class OxyParams
+    {
+        
+        public string source { get; set; }
+        public string domain { get; set; }
+        public string[] query { get; set; }
+        public int limit { get; set; }
+        public int pages { get; set; }
+        public int start_page { get; set; }
+        public string locale { get; set; }
+        public string geo_location { get; set; }
+        public bool parse { get; set; }
+        public string user_agent_type { get; set; }
+        public string[] url { get; set; }
+        public string callback_url { get; set; }
+        public List<Context> context { get; set; }
+
+    }
+    class Context
+    {
+        public string key { get; set; }
+        public dynamic value { get; set; }
+
+        public Context(string key, dynamic value)
+        {
+            this.key = key;
+            this.value = value;
+        }
+    }
+}

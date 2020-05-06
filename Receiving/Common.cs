f56a93a96bfbb4cb6fb050eed8b9f8b6141d7bcb
Prakash <prakash@intelligentpositioning.com>
2020-05-06 22:40:25 +0530

    updated..

---
+++ Receiving/Common.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace Receiving
+{
+    class Common
+    {
+        internal static string ReadConnection()
+        {
+            try
+            {
+                XmlDocument xml = new XmlDocument();
+                string fileName = @"C:\Inetpub\wwwroot\ServerIP1.xml";
+                // You'll need to put the correct path to your xml file here
+                xml.Load(fileName);
+
+                // Select a specific node
+                XmlNode node = xml.SelectSingleNode("ConnectionString/con");
+                // Get its value
+                string name = node.InnerText;
+
+                return name;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        internal static string readAPI()
+        {
+            try
+            {
+                XmlDocument xml = new XmlDocument();
+                string fileName = @"C:\Inetpub\wwwroot\ServerIP1.xml";
+                //string fileName = @"C:\Inetpub\wwwroot\ServerIP_Callback.xml";
+                // You'll need to put the correct path to your xml file here
+                xml.Load(fileName);
+                // Select a specific node
+                XmlNode node = xml.SelectSingleNode("ConnectionString/apiNew");
+                // Get its value
+                string name = node.InnerText;
+                return name;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        internal static string strConn()
+        {
+            try
+            {
+                XmlDocument xml = new XmlDocument();
+                string fileName = @"C:\Inetpub\wwwroot\ServerIP1.xml";
+                // You'll need to put the correct path to your xml file here
+                xml.Load(fileName);
+
+                // Select a specific node
+                XmlNode node = xml.SelectSingleNode("ConnectionString/con");
+                // Get its value
+                string name = node.InnerText;
+                return name;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        //internal static string ReadConnection()
+        //{
+        //    XmlDocument xml = new XmlDocument();
+        //    string fileName = @"C:\Inetpub\wwwroot\TrendingLiveAPI.xml";
+        //    // You'll need to put the correct path to your xml file here
+        //    xml.Load(fileName);
+
+        //    // Select a specific node
+        //    XmlNode node = xml.SelectSingleNode("TrendingAPI/con");
+        //    // Get its value
+        //    string name = node.InnerText.Trim();
+
+        //    return name;
+        //}
+
+        internal static int GetOxylabsTime()
+        {
+            int time = 0;
+            string strQuery = "exec [dbo].[GetOxylabsTime]";
+            SqlConnection objCon = new SqlConnection(ReadConnection());
+            try
+            {
+                objCon.Open();
+                SqlCommand objCmd = new SqlCommand(strQuery, objCon);
+                objCmd.CommandTimeout = 0;
+                SqlDataReader objData = null;
+                objData = objCmd.ExecuteReader(CommandBehavior.CloseConnection);
+                while (objData.Read())
+                {
+                    time = Convert.ToInt32(objData[0]);
+                }
+                objData.Close();
+            }
+            catch (SqlException e)
+            {
+                string errMsg = "Database Connection is temporarily not working\n" + e.ToString();
+            }
+            finally
+            {
+                if (objCon.State == ConnectionState.Open)
+                {
+                    objCon.Close();
+                }
+            }
+            return time;
+        }
+
+        internal static int GetOxylabsCount()
+        {
+            int count = 0;
+            string strQuery = "exec [dbo].[GetOxylabsCount]";
+            SqlConnection objCon = new SqlConnection(ReadConnection());
+            try
+            {
+                objCon.Open();
+                SqlCommand objCmd = new SqlCommand(strQuery, objCon);
+                objCmd.CommandTimeout = 0;
+                SqlDataReader objData = null;
+                objData = objCmd.ExecuteReader(CommandBehavior.CloseConnection);
+                while (objData.Read())
+                {
+                    count = Convert.ToInt16(objData[0]);
+                }
+                objData.Close();
+            }
+            catch (SqlException e)
+            {
+                string errMsg = "Database Connection is temporarily not working\n" + e.ToString();
+            }
+            finally
+            {
+                if (objCon.State == ConnectionState.Open)
+                {
+                    objCon.Close();
+                }
+            }
+            return count;
+        }
+    }
+}
+
--- Receiving/HTMLParserNewTask.cs
+++ Receiving/HTMLParserNewTask.cs
@@ -21,83 +21,80 @@ namespace Receiving
         string myDate = DateTime.Today.ToString("yyyy-MM-dd");
         public event KeywordDone OnKeywordDone;
         public delegate void KeywordDone(string value);
-        //public HTMLParserNewTask()
-        //{
-        //    Thread t1 = new Thread(new ThreadStart(StartProcess))
-        //    {
-        //        Name = "YahooHK_Desktop"
-        //    };
-        //    t1.Start();
-        //}
 
-        
 
         public string Gethtml(int seid, string kw, ArrayList al)
         {
             string response = "";
             string html = "";
-            string status = "done";            
+            string status = "done";
             string resultsurl = "";
             string jobid = "";
-            for (int i = 0; i < al.Count; i++)
+            try
             {
-                //jobid = al[i].ToString().Replace("http://data.oxylabs.io/v1/queries/", "").Replace("/results", "");
-                resultsurl = al[i].ToString();
-                
-
-                try
-            {
-                string username = "gpidatametrics";
-                string password = "sdV5X3fcX6";
-
-                if (status == "done")
+                for (int i = 0; i < al.Count; i++)
                 {
-                    string resURL = resultsurl;
-                    HttpWebRequest httpWebRequest = (HttpWebRequest)WebRequest.Create(resURL);
-                    string authInfo = Convert.ToBase64String(Encoding.Default.GetBytes(username + ":" + password));
-                    httpWebRequest.Headers["Authorization"] = "Basic " + authInfo;
-                    HttpWebResponse res = (HttpWebResponse)httpWebRequest.GetResponse();
-                    Stream resStream = res.GetResponseStream();
-                    StreamReader reader = new StreamReader(resStream, Encoding.UTF8);
-                    response = reader.ReadToEnd();
-                    resStream.Close();
-                    res.Close();
-                    ArrayList arRes = new ArrayList();
+                    jobid = al[i].ToString().Replace("http://data.oxylabs.io/v1/queries/", "").Replace("/results", "");
+                    resultsurl = al[i].ToString();
+
                     try
                     {
-                        
-                        JObject obj = JObject.Parse(response);
-                        var cont = obj["results"];
-                        foreach (JObject jo in cont)
+                        string username = "gpidatametrics";
+                        string password = "sdV5X3fcX6";
+
+                        if (status == "done")
                         {
-                            ArrayList addURLs = new ArrayList();
-                            html+= jo["content"].Value<string>();
-                            //System.IO.File.WriteAllText(@"c:\inetpub\wwwroot\html\" + kw + ".html", response, Encoding.UTF8);
+                            string resURL = resultsurl;
+                            HttpWebRequest httpWebRequest = (HttpWebRequest)WebRequest.Create(resURL);
+                            string authInfo = Convert.ToBase64String(Encoding.Default.GetBytes(username + ":" + password));
+                            httpWebRequest.Headers["Authorization"] = "Basic " + authInfo;
+                            HttpWebResponse res = (HttpWebResponse)httpWebRequest.GetResponse();
+                            Stream resStream = res.GetResponseStream();
+                            StreamReader reader = new StreamReader(resStream, Encoding.UTF8);
+                            response = reader.ReadToEnd();
+                            resStream.Close();
+                            res.Close();
+                            ArrayList arRes = new ArrayList();
+                            try
+                            {
+
+                                JObject obj = JObject.Parse(response);
+                                var cont = obj["results"];
+                                foreach (JObject jo in cont)
+                                {
+                                    ArrayList addURLs = new ArrayList();
+                                    html += jo["content"].Value<string>();
+                                    //System.IO.File.WriteAllText(@"c:\inetpub\wwwroot\html\" + kw + ".html", response, Encoding.UTF8);
+                                }
+                            }
+                            catch (Exception ex)
+                            {
+                                string error = ex.Message.ToString();
+                            }
 
                         }
                     }
+
                     catch (Exception ex)
                     {
-                        //throw ex;
+                        Console.WriteLine("Result Request: " + ex.Message);
+                        if (!string.IsNullOrEmpty(jobid))
+                        {
+                            try
+                            {
+                                ProcessError(kw, seid, jobid);
+                            }
+                            finally { }
+                        }
+                        OnKeywordDone.Invoke("Error:  seid: " + seid + ",  keyword: " + kw + ",  jobid: " + jobid + "\r\n\t" + ex.Message);
                     }
-                    
                 }
             }
-                
             catch (Exception ex)
             {
-                Console.WriteLine("Result Request: " + ex.Message);
-                if (!string.IsNullOrEmpty(jobid))
-                {
-                    try
-                    {
-                        ProcessError(kw, seid, jobid);
-                    }
-                    finally { }
-                }
-                OnKeywordDone.Invoke("Error:  seid: " + seid + ",  keyword: " + kw + ",  jobid: " + jobid + "\r\n\t" + ex.Message);
+                string error = ex.Message.ToString();
             }
-            }
+
             return html;
         }
 
@@ -106,7 +103,7 @@ namespace Receiving
             string qry = "insert into dashboard_dataerrors (date, name, seid, jobid) values(Convert(varchar(10),'" + myDate + "',103), N'" +
                 kw.Replace("'", "''") + "', " + seid + ", '" + jobid + "' )";
 
-            using (SqlConnection con = new SqlConnection(strConn()))
+            using (SqlConnection con = new SqlConnection(Common.strConn()))
             {
                 try
                 {
@@ -136,313 +133,6 @@ namespace Receiving
                 {
                     throw ex;
                 }
-            }
-        }
-
-        public string strConn()
-        {
-            try
-            {
-                XmlDocument xml = new XmlDocument();
-                string fileName = @"C:\Inetpub\wwwroot\Callback_TrackingTrending.xml";
-                // You'll need to put the correct path to your xml file here
-                xml.Load(fileName);
-
-                // Select a specific node
-                XmlNode node = xml.SelectSingleNode("ConnectionString/con");
-                // Get its value
-                string name = node.InnerText;
-
-                return name;
-            }
-            catch (Exception ex)
-            {
-                throw ex;
-            }
-        }
-        private void ProcessResults(ArrayList alRes, string kw, int seid, string jobid)
-        {
-            string qry = "";
-
-            if (alRes.Count < 1)
-            {
-                Console.WriteLine("There is no result for " + kw);
-                throw new Exception("No Results");
-            }
-            else
-            {
-                Console.WriteLine("Processing the keyword: " + kw);
-
-                string tname = Thread.CurrentThread.Name;
-
-                string path = @"C:\Inetpub\wwwroot\oxycallback" + tname + ".xml";
-
-                string result = string.Empty;
-                try
-                {
-                    MemoryStream stream = new MemoryStream();
-                    using (XmlTextWriter writer = new XmlTextWriter(stream, Encoding.UTF8))
-                    {
-                        writer.Formatting = System.Xml.Formatting.Indented;
-                        writer.Indentation = 2;
-                        writer.WriteStartDocument();
-
-                        writer.WriteStartElement("", "searchResults", "");
-
-                        writer.WriteStartElement("", "searchResult", "");
-                        writer.WriteStartAttribute("searchEngineId");
-                        writer.WriteString(seid.ToString());
-                        writer.WriteStartAttribute("keyword");
-                        writer.WriteString(kw);
-                        writer.WriteStartAttribute("date");
-                        writer.WriteString(myDate);
-                        string c = string.Empty;
-                        int k;
-
-                        for (int i = 0; i < alRes.Count; i++)
-                        {
-                            k = i + 1;
-                            c = k.ToString();
-                            if (alRes[i].ToString().Contains("e100") && alRes[i].ToString().Trim().StartsWith("e100"))
-                                continue;
-                            writer.WriteStartElement("", "url", "");
-                            writer.WriteStartAttribute("position");
-                            writer.WriteString(c);
-                            writer.WriteEndAttribute();
-                            string dURL = alRes[i].ToString();
-                            writer.WriteString(dURL);
-                            writer.WriteEndElement();
-                            //Insert100DashBoardData(myDate, kw, seid.ToString(),k, alRes[i].ToString());
-                            qry += "insert into dashboard_data4(date,name,seid,position,url)values(Convert(varchar(10),'" + myDate + "',103),N'" + kw.Replace("'", "''") + "'," + seid + ",'" + c + "',N'" + dURL.ToString().Replace("'", "''") + "')";
-                        }
-                        writer.WriteEndElement();
-                        writer.WriteEndElement();
-                        writer.WriteEndDocument();
-
-                        writer.Flush();
-                        writer.Flush();
-                        //writer.Close();
-                        Encoding utf = Encoding.UTF8;
-                        result = utf.GetString(stream.GetBuffer(), 0, (int)stream.Length);
-                        stream.Close();
-                    }
-
-                    if (!string.IsNullOrEmpty(result))
-                    {
-                        StreamWriter sw = new StreamWriter(path, false);
-                        sw.Write(result);
-                        sw.Close();
-                    }
-                    if (!string.IsNullOrEmpty(result))
-                    {
-                        try
-                        {
-                            if (alRes.Count > 50)
-                            {
-                                SendXmlToAPI(path);
-                                Insert100DashBoardData(qry); 
-                                InsertDashBoardData(kw, seid, alRes.Count, alRes[0].ToString(), jobid);
-                            }
-                        }
-                        catch (Exception ex)
-                        {
-                            throw ex;
-                        }
-                    }
-                }
-                catch (Exception ex)
-                {
-                    throw ex;
-                }
-            }
-        }
-
-        private void SendXmlToAPI(string path)
-        {
-            string submitURL = readAPI();
-
-            //string user = "pi-tracking";
-            //string pwd = "ipseo2001";
-
-            string user = "pisoftware";
-            string pwd = "r00t123456";
-
-            try
-            {
-                HttpWebRequest httpWReq = (HttpWebRequest)WebRequest.Create(submitURL);
-                //httpWReq = (HttpWebRequest)WebRequest.Create(submitURL);
-                httpWReq.UseDefaultCredentials = true;
-                httpWReq.PreAuthenticate = true;
-                httpWReq.Credentials = CredentialCache.DefaultCredentials;
-
-                Encoding encoding = new UTF8Encoding();
-                string postData = GetTextFromXMLFile(path);
-                byte[] data = encoding.GetBytes(postData);
-
-                httpWReq.ProtocolVersion = HttpVersion.Version11;
-                httpWReq.Method = "POST";
-                httpWReq.ContentType = "application/x-www-form-urlencoded"; //charset=UTF-8";  
-
-
-                string auth = string.Format("{0}:{1}", user, pwd);
-                string enc = Convert.ToBase64String(Encoding.ASCII.GetBytes(auth));
-                string cred = string.Format("{0} {1}", "Basic", enc);
-
-
-                httpWReq.Headers[HttpRequestHeader.Authorization] = cred;
-                httpWReq.ContentLength = data.Length;
-
-
-                Stream stream = httpWReq.GetRequestStream();
-                stream.Write(data, 0, data.Length);
-                stream.Close();
-
-                HttpWebResponse response = (HttpWebResponse)httpWReq.GetResponse();
-                string s = response.ToString();
-                StreamReader reader = new StreamReader(response.GetResponseStream());
-
-                String xmlResponse = "";
-                String temp = null;
-                while ((temp = reader.ReadLine()) != null)
-                {
-                    xmlResponse += temp;
-                }
-                reader.Close();
-                response.Close();
-            }
-            catch (WebException ex)
-            {
-                string errorMsg = string.Empty;
-                using (WebResponse response = ex.Response)
-                {
-                    HttpWebResponse httpResponse = (HttpWebResponse)response;
-                    errorMsg = string.Format("API Error: StatusCode {0}", httpResponse.StatusCode);
-
-                    using (Stream data = response.GetResponseStream())
-                    using (var reader = new StreamReader(data))
-                    {
-                        errorMsg += "\r\n" + reader.ReadToEnd();
-                    }
-                }
-                throw new Exception(errorMsg);
-            }
-        }
-
-        private string GetTextFromXMLFile(string file)
-        {
-            StreamReader reader = new StreamReader(file);
-            string ret = reader.ReadToEnd();
-            reader.Close();
-            return ret;
-        }
-
-        public string readAPI()
-        {
-            try
-            {
-                XmlDocument xml = new XmlDocument();
-                string fileName = @"C:\Inetpub\wwwroot\Callback_TrackingTrending.xml";
-                // You'll need to put the correct path to your xml file here
-                xml.Load(fileName);
-
-                // Select a specific node
-                XmlNode node = xml.SelectSingleNode("ConnectionString/apiSubmit");
-                // Get its value
-                string name = node.InnerText;
-
-                return name;
-            }
-            catch (Exception ex)
-            {
-                throw ex;
-            }
-        }
-
-        void Insert100DashBoardData(string qry)
-        {
-            //string strInsert = "insert into dashboard_yandex(date,name,seid,position,url)values(Convert(varchar(10),'" + ddate + "',103),N'" + kwd.Replace("'", "''") + "'," + seid + ",'"+ position +"',N'" + url.Replace("'", "''") + "')";
-
-            try
-            {
-                using (SqlConnection con = new SqlConnection(strConn()))
-                {
-                    con.Open();
-                    using (SqlCommand comm = new SqlCommand(qry, con))
-                    {
-                        comm.CommandTimeout = 0;
-                        comm.ExecuteNonQuery();
-
-                    }
-                }
-            }
-            catch (SqlException ex)
-            {
-                string errorMessage = "Database Error: \r\n";
-                for (int i = 0; i < ex.Errors.Count; i++)
-                {
-                    errorMessage += "Index #" + i + "\n" +
-                                     "Message: " + ex.Errors[i].Message + "\n" +
-                                     "LineNumber: " + ex.Errors[i].LineNumber + "\n" +
-                                     "Source: " + ex.Errors[i].Source + "\n" +
-                                     "Procedure: " + ex.Errors[i].Procedure + "\n" +
-                                     "Server: " + ex.Errors[i].Server + "\n";
-                }
-
-                throw new Exception(errorMessage);
-            }
-            catch (Exception ex)
-            {
-                throw ex;
-            }
-
-        }
-
-        private void InsertDashBoardData(string kw, int seid, int cnt, string alRes, string jobid)
-        {
-            string qry = "insert into dashboard_data(date, name, seid, jobid, count, url) " +
-                    "values(Convert(varchar(10),'" + myDate + "',103),N'" + kw.Replace("'", "''") + "', " + seid + ", '" + jobid + "', " + cnt + ", N'" + alRes.Replace("'", "''") + "')";
-
-            try
-            {
-                using (SqlConnection con = new SqlConnection(strConn()))
-                {
-                    con.Open();
-                    using (SqlCommand comm = new SqlCommand(qry, con))
-                    {
-                        comm.CommandTimeout = 0;
-                        comm.ExecuteNonQuery();
-
-
-
-                        //if (lt20)
-                        //{
-                        //    string url = alRes.Count > 0 ? alRes[0].ToString() : "";
-                        //    qry = "exec [InsertLessthan20] '" + myDate + "',N'" + kw.Replace("'", "''") + "'," + seid + ",N'" + url.Replace("'", "''") + "'," + alRes.Count + ",'" + jobid + "'";
-                        //    comm.CommandText = qry;
-                        //    comm.CommandType = CommandType.Text;
-                        //    comm.ExecuteNonQuery();
-                        //}
-                    }
-                }
-            }
-            catch (SqlException ex)
-            {
-                string errorMessage = "Database Error: \r\n";
-                for (int i = 0; i < ex.Errors.Count; i++)
-                {
-                    errorMessage += "Index #" + i + "\n" +
-                                     "Message: " + ex.Errors[i].Message + "\n" +
-                                     "LineNumber: " + ex.Errors[i].LineNumber + "\n" +
-                                     "Source: " + ex.Errors[i].Source + "\n" +
-                                     "Procedure: " + ex.Errors[i].Procedure + "\n" +
-                                     "Server: " + ex.Errors[i].Server + "\n";
-                }
-
-                throw new Exception(errorMessage);
-            }
-            catch (Exception ex)
-            {
-                throw ex;
             }
         }
 
